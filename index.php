5068688c446182311c6cee1aa51656a51d115746
uzairmzln <78578334+uzairmzln@users.noreply.github.com>
2021-02-05 11:41:42 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<? 
+
+//Array Jujukan 
+$result = array ( //array pertama = pelajar
+"Riyad" => array ('KPD4015'=>80 'SEJARAH'=>80, 'DISCRETE MATH'=>90, 'KPD4024'=>85, 'P.ISLAM=>90),
+"Hamsyari" => array ('kpd4015'=>70, 'SEJARAH'=>75, 'DISCRETE MATH'=>92, 'KPD4024'=>87, 'P.ISLAM'=>91),
+"FATIN ADLIN" => array ('kpd4015'=>90, 'SEJARAH'=>92, 'DISCRETE MATH'=>73, 'KPD4024'=>89, 'P.ISLAM'=>78),
+"FAHEEM AIDEED" => array ('kpd4015'=>60, 'SEJARAH'=>72, 'DISCRETEMATH'=>83, 'KPD4024'=>79, 'P.ISLAM'=>75),
+"NADIA NABILA" => array ('kpd4015'=>96, 'SEJARAH'=>92, 'DISCRETE MATH'=>93, 'KPD4024'=>82, 'P.ISLAM'=>76)
+	);
+foreach ($result as $Pelajar => $value {
+	{
+		echo "Pelajar:  .$Pelajar
+		echo "<br>";
+
+	} foreach ($Subjek as $subjek => $value)
+		{
+			echo "Subjek:".$subjek." Result:".$rst;
+			echo "<br>";
+
+		}
+		 echo "_ _ _ _ _ _ _ _ _ _ _ _ _ _ _";
+		echo "<br>";
+	
+
+
+?
